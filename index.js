38aed903ad00394323d533ae2ab23889525f407a
profrafamatos <rcmm@cesar.school>
2024-10-13 13:18:32 -0300

    resolução dos exercícios

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 /**** Escreva o código abaixo 👇******/
+function somar(a, b) {
+  return a + b;
+}
 
+function multiplicar(a, b) {
+  return a * b;
+}
 
+function subtrair(a, b) {
+  return a - b;
+}
 
+function dividir(a, b) {
+  if (b === 0) {
+    throw new Error("Erro: divisão por zero")
+  }
+  return a / b
+}
 
 /**** Escreva o código acima ☝️******/
 
